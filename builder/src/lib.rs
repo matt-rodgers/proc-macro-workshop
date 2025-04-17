71b982920d2e93c14342be1a7fdc056b14ad10fa
Matt Rodgers <mattrodgers31@gmail.com>
2025-04-17 08:12:02 +0100

    refactor current code

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,7 +1,6 @@
 use proc_macro::TokenStream;
-use proc_macro2::Ident;
 use quote::quote;
-use syn::{parse_macro_input, Data, DeriveInput};
+use syn::{parse_macro_input, DeriveInput};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
@@ -9,29 +8,23 @@ pub fn derive(input: TokenStream) -> TokenStream {
 
     // The identifier of the top level struct we're operating on
     let struct_ident = input.ident;
-    let builder_ident = Ident::new(&format!("{}Builder", struct_ident), struct_ident.span());
+    let builder_ident = syn::Ident::new(&format!("{}Builder", struct_ident), struct_ident.span());
 
-    let data = if let Data::Struct(data) = input.data {
+    let data = if let syn::Data::Struct(data) = input.data {
         data
     } else {
         panic!("Expected Data::Struct")
     };
 
     // Replace each struct field with the same type wrapped in an Option
-    let struct_fields = data.fields.iter().map(|f| {
+    let builder_fields = data.fields.iter().map(|f| {
         let name = &f.ident;
         let ty = &f.ty;
         quote! { #name: Option<#ty> }
     });
 
-    let builder_struct = quote! {
-        pub struct #builder_ident {
-            #(#struct_fields,)*
-        }
-    };
-
     // Initialise each struct field to None
-    let struct_initialisers = data.fields.iter().map(|f| {
+    let builder_initialisers = data.fields.iter().map(|f| {
         let name = &f.ident;
         quote! { #name: None }
     });
@@ -48,43 +41,35 @@ pub fn derive(input: TokenStream) -> TokenStream {
         }
     });
 
-    // Create build method to turn the Builder into the original struct
+    // For each field in the builder, set the appropriate field in the original struct
     let set_fields = data.fields.iter().map(|f| {
         let name = &f.ident;
         let err_msg = format!("Field '{}' is not set", name.as_ref().unwrap());
         quote! { #name: self.#name.clone().ok_or_else(|| #err_msg.to_string())? }
     });
 
-    let build_method = quote! {
-        pub fn build(&mut self) -> Result<#struct_ident, Box<dyn std::error::Error>> {
-            Ok(#struct_ident {
-                #(#set_fields,)*
-            })
-        }
-    };
-
-    let builder_impl = quote! {
-        impl #builder_ident {
-            #(#builder_methods)*
-            #build_method
-        }
-    };
-
-    // Create a builder() method on the original struct
-    let struct_impl = quote! {
+    quote! {
         impl #struct_ident {
             pub fn builder() -> #builder_ident {
                 #builder_ident {
-                    #(#struct_initialisers,)*
+                    #(#builder_initialisers,)*
                 }
             }
         }
-    };
 
-    quote! {
-        #struct_impl
-        #builder_struct
-        #builder_impl
+        pub struct #builder_ident {
+            #(#builder_fields,)*
+        }
+
+        impl #builder_ident {
+            #(#builder_methods)*
+
+            pub fn build(&mut self) -> Result<#struct_ident, Box<dyn std::error::Error>> {
+                Ok(#struct_ident {
+                    #(#set_fields,)*
+                })
+            }
+        }
     }
     .into()
 }
