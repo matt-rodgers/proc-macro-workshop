6776b81c6fc98955046486fd107c7ddf4b0a4146
Matt Rodgers <mattrodgers31@gmail.com>
2025-04-24 07:54:00 +0100

    sorted part 3

--- main.rs
+++ main.rs
@@ -6,24 +6,14 @@
 // To run the code:
 //     $ cargo run
 
-use seq::seq;
+use sorted::sorted;
 
-seq!(N in 16..=20 {
-    enum E {
-        #(
-            Variant~N,
-        )*
-    }
-});
-
-fn main() {
-    let e = E::Variant16;
-
-    let desc = match e {
-        E::Variant16 => "min",
-        E::Variant17 | E::Variant18 | E::Variant19 => "in between",
-        E::Variant20 => "max",
-    };
-
-    assert_eq!(desc, "min");
+#[sorted]
+pub enum Error {
+    ThatFailed,
+    ThisFailed,
+    SomethingFailed,
+    WhoKnowsWhatFailed,
 }
+
+fn main() {}
--- sorted/src/lib.rs
+++ sorted/src/lib.rs
@@ -1,5 +1,5 @@
 use proc_macro::TokenStream;
-use quote::{quote, ToTokens};
+use quote::ToTokens;
 use syn::{parse_macro_input, Result};
 
 #[proc_macro_attribute]
@@ -7,23 +7,46 @@ pub fn sorted(args: TokenStream, input: TokenStream) -> TokenStream {
     let item = parse_macro_input!(input as syn::Item);
 
     sorted_inner(args, item)
-        .unwrap_or_else(|err| err.to_compile_error().into())
+        .unwrap_or_else(|err| err.to_compile_error())
         .into()
 }
 
 fn sorted_inner(args: TokenStream, item: syn::Item) -> Result<proc_macro2::TokenStream> {
-    let _ = args;
+    // We don't expect any args for now
+    assert!(args.is_empty());
 
     let enum_item = match item {
         syn::Item::Enum(en) => en,
         _ => {
-            return Ok(quote! {
-                compile_error!("expected enum or match expression");
-            })
+            return Err(syn::Error::new(
+                proc_macro2::Span::call_site(),
+                "expected enum or match expression",
+            ));
         }
     };
 
-    eprintln!("{:#?}", enum_item);
+    let mut names: Vec<String> = Vec::new();
+    for variant in enum_item.variants.iter() {
+        let name = variant.ident.to_string();
+        if let Some(last) = names.last() {
+            if &name < last {
+                // We are out of order, find where the variant *should* be placed
+                match names.binary_search(&name) {
+                    Ok(_) => {
+                        return Err(syn::Error::new_spanned(variant, "duplicate enum variant"));
+                    }
+                    Err(n) => {
+                        return Err(syn::Error::new_spanned(
+                            variant,
+                            format!("{} should sort before {}", name, names[n]),
+                        ));
+                    }
+                }
+            }
+        }
+
+        names.push(name);
+    }
 
     Ok(enum_item.to_token_stream())
 }
