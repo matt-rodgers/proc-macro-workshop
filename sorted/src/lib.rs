9dee0eee8b2b50d97bad639bb1e26eb4a5be573a
Matt Rodgers <mattrodgers31@gmail.com>
2025-04-24 07:31:55 +0100

    sorted part 1 & 2

--- sorted/src/lib.rs
+++ sorted/src/lib.rs
@@ -1,9 +1,29 @@
 use proc_macro::TokenStream;
+use quote::{quote, ToTokens};
+use syn::{parse_macro_input, Result};
 
 #[proc_macro_attribute]
 pub fn sorted(args: TokenStream, input: TokenStream) -> TokenStream {
-    let _ = args;
-    let _ = input;
+    let item = parse_macro_input!(input as syn::Item);
 
-    unimplemented!()
+    sorted_inner(args, item)
+        .unwrap_or_else(|err| err.to_compile_error().into())
+        .into()
+}
+
+fn sorted_inner(args: TokenStream, item: syn::Item) -> Result<proc_macro2::TokenStream> {
+    let _ = args;
+
+    let enum_item = match item {
+        syn::Item::Enum(en) => en,
+        _ => {
+            return Ok(quote! {
+                compile_error!("expected enum or match expression");
+            })
+        }
+    };
+
+    eprintln!("{:#?}", enum_item);
+
+    Ok(enum_item.to_token_stream())
 }
