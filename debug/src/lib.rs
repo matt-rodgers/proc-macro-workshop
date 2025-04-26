59572b3f376a27a0e89ae69f11ca6918a8a2568e
Matt Rodgers <mattrodgers31@gmail.com>
2025-04-26 11:48:14 +0100

    debug part 3

--- debug/src/lib.rs
+++ debug/src/lib.rs
@@ -2,21 +2,52 @@ use proc_macro::TokenStream;
 use quote::quote;
 use syn::{parse_macro_input, DeriveInput};
 
-#[proc_macro_derive(CustomDebug)]
+#[proc_macro_derive(CustomDebug, attributes(debug))]
 pub fn derive(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
     let name = &input.ident;
     let name_str = format!("{}", name);
 
-    let field_idents = match input.data {
-        syn::Data::Struct(ref s) => s.fields.iter().filter_map(|f| f.ident.as_ref()),
-        _ => todo!(),
+    let fields = match input.data {
+        syn::Data::Struct(ref s) => &s.fields,
+        _ => {
+            let err =
+                syn::Error::new_spanned(name, "CustomDebug is only implemented for DataStructs");
+            return err.to_compile_error().into();
+        }
     };
 
-    let field_calls = field_idents.map(|id| {
-        let id_str = format!("{}", id);
-        quote! {
-            .field(#id_str, &self.#id)
+    let field_calls = fields.iter().map(|f| {
+        if let Some(ref id) = f.ident {
+            let mut custom_format = None;
+
+            for attr in f.attrs.iter() {
+                if let syn::Meta::NameValue(ref nv) = attr.meta {
+                    if nv.path.segments.len() == 1 && nv.path.segments[0].ident == "debug" {
+                        if let syn::Expr::Lit(syn::ExprLit {
+                            lit: syn::Lit::Str(ref ls),
+                            ..
+                        }) = nv.value
+                        {
+                            custom_format = Some(ls.value());
+                        }
+                    }
+                }
+            }
+
+            let id_str = id.to_string();
+
+            if let Some(custom) = custom_format {
+                quote! {
+                    .field(#id_str, &format_args!(#custom, &self.#id))
+                }
+            } else {
+                quote! {
+                    .field(#id_str, &self.#id)
+                }
+            }
+        } else {
+            quote! {}
         }
     });
 
