e9c3d9ecfd66d33c23a15aa15fb11685c2ff7c70
Matt Rodgers <mattrodgers31@gmail.com>
2025-04-28 21:22:00 +0100

    debug part 7

--- debug/src/lib.rs
+++ debug/src/lib.rs
@@ -53,44 +53,61 @@ pub fn derive(input: TokenStream) -> TokenStream {
         }
     });
 
-    // Find all of the field types which are contained in a PhantomData<T>
-    let phantom: Vec<_> = fields
-        .iter()
-        .filter_map(|f| {
-            if let syn::Type::Path(ref p) = f.ty {
-                for seg in p.path.segments.iter() {
-                    if seg.ident == "PhantomData" {
-                        if let syn::PathArguments::AngleBracketed(
-                            syn::AngleBracketedGenericArguments { ref args, .. },
-                        ) = seg.arguments
+    let mut generics = input.generics.clone();
+    let mut associated_types = Vec::new();
+
+    // Ensure that the required type generics implement Debug.
+    // A type need not implement Debug if it only appears:
+    // - inside a PhantomData<T>, or
+    // - as part of an associated type, in which case the associated type must implement Debug
+    for param in &mut generics.params {
+        if let syn::GenericParam::Type(ref mut type_param) = *param {
+            if fields.iter().any(|f| {
+                match find_type_path_with_ident(&f.ty, &type_param.ident) {
+                    None => {
+                        // This field doesn't contain the ident we're currently considering
+                        return false;
+                    }
+                    Some(p) => {
+                        if p.path.segments.len() > 1 && p.path.segments[0].ident == type_param.ident
                         {
-                            for arg in args.iter() {
-                                if let syn::GenericArgument::Type(syn::Type::Path(ref ty)) = arg {
-                                    assert!(ty.path.segments.len() == 1);
-                                    return Some(&ty.path.segments[0].ident);
-                                }
-                            }
+                            associated_types.push(syn::Type::Path(p.clone()));
+                            // The ident is only present as part of an associated type
+                            return false;
                         }
                     }
                 }
-            }
-            None
-        })
-        .collect();
 
-    // Ensure that every type generic implements Debug
-    let mut generics = input.generics.clone();
-    for param in &mut generics.params {
-        if let syn::GenericParam::Type(ref mut type_param) = *param {
-            // The exception is if the type in inside a PhantomData, in which case it need not
-            // implement debug
-            if !phantom.iter().any(|ty| **ty == type_param.ident) {
+                if is_phantom_data(&f.ty, &type_param.ident) {
+                    return false;
+                }
+
+                // By default a type must implement Debug, if none of the other conditions were met
+                true
+            }) {
                 type_param.bounds.push(parse_quote!(::std::fmt::Debug));
             }
         }
     }
 
     let (impl_generics, ty_generics, where_clause) = generics.split_for_impl();
+
+    // Generate a where clause for any associated types
+    let where_clause = if associated_types.len() > 0 {
+        let mut new_where_clause = where_clause
+            .cloned()
+            .unwrap_or_else(|| parse_quote! { where });
+
+        new_where_clause
+            .predicates
+            .extend(associated_types.iter().map(|ty| -> syn::WherePredicate {
+                parse_quote! { #ty: ::std::fmt::Debug }
+            }));
+
+        Some(new_where_clause)
+    } else {
+        where_clause.cloned()
+    };
 
     quote! {
         impl #impl_generics ::std::fmt::Debug for #name #ty_generics #where_clause {
@@ -102,4 +119,62 @@ pub fn derive(input: TokenStream) -> TokenStream {
         }
     }
     .into()
+}
+
+fn is_phantom_data(ty: &syn::Type, ident: &syn::Ident) -> bool {
+    if let syn::Type::Path(ref p) = ty {
+        for seg in p.path.segments.iter() {
+            if seg.ident == "PhantomData" {
+                if let syn::PathArguments::AngleBracketed(syn::AngleBracketedGenericArguments {
+                    ref args,
+                    ..
+                }) = seg.arguments
+                {
+                    assert!(args.len() == 1);
+                    let arg = args.first().unwrap();
+                    if let syn::GenericArgument::Type(syn::Type::Path(ref inner_ty)) = arg {
+                        if inner_ty.path.segments.iter().any(|seg| &seg.ident == ident) {
+                            return true;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    false
+}
+
+/// Search for an appearance of the given ident within a (potentially nested) type
+/// If found, return the whole type path that contains it.
+/// For example:
+///   ty: Vec<T>, ident: T --> Some(T)
+///   ty: Vec<T::Value>, ident: T --> Some(T::Value)
+fn find_type_path_with_ident<'a>(
+    ty: &'a syn::Type,
+    ident: &syn::Ident,
+) -> Option<&'a syn::TypePath> {
+    if let syn::Type::Path(ref p) = ty {
+        if let Some(ref seg) = p.path.segments.last() {
+            if let syn::PathArguments::AngleBracketed(syn::AngleBracketedGenericArguments {
+                ref args,
+                ..
+            }) = seg.arguments
+            {
+                for arg in args {
+                    if let syn::GenericArgument::Type(ref inner_ty) = arg {
+                        let inner_is_assoc = find_type_path_with_ident(inner_ty, ident);
+                        if inner_is_assoc.is_some() {
+                            return inner_is_assoc;
+                        }
+                    }
+                }
+            }
+        }
+
+        if p.path.segments.iter().any(|seg| &seg.ident == ident) {
+            return Some(p);
+        }
+    }
+
+    None
 }
